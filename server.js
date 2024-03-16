855345d14b5f5ae175853701ba9ddcf9d1be4b38
Ishan <ishannaik7@gmail.com>
2024-03-16 16:49:05 +0530

    Changed a few things

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 const Document = require("./models/Document");
