24d7aeb5c1c2bf1f26f72a1aa3b8744f2712956d
Ishan <ishannaik7@gmail.com>
2024-03-16 16:46:43 +0530

    Added env port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -52,6 +53,6 @@ app.get("/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
